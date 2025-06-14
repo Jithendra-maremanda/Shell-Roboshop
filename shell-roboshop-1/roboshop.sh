b85a2fe865d0aec2dd723891425bf98f084f55c0
Jithendra-maremanda <jithendramaremanda@gmail.com>
2025-06-15 01:06:22 +0530

    new files are added

--- shell-roboshop-1/roboshop.sh
+++ shell-roboshop-1/roboshop.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z07875261XFWESB6HKZDD" # replace with your ZONE ID
 DOMAIN_NAME="stansrobotshop.site" # replace with your domain
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+# for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c3960387c5a6b9de --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
